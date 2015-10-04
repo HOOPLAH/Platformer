50dbe79a784a94a2bd1d0213514d8264ba2da14d
Lucas DeRego <lucas.derego@gmail.com>
2015-10-03 22:10:49 -1000

    started ladders

---
+++ include/Ladder.h
@@ -0,0 +1,14 @@
+#ifndef LADDER_H
+#define LADDER_H
+
+
+class Ladder
+{
+    public:
+        Ladder();
+        virtual ~Ladder();
+    protected:
+    private:
+};
+
+#endif // LADDER_H
---
+++ src/Ladder.cpp
@@ -0,0 +1,11 @@
+#include "Ladder.h"
+
+Ladder::Ladder()
+{
+    //ctor
+}
+
+Ladder::~Ladder()
+{
+    //dtor
+}
